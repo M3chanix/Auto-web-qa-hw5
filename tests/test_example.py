0f6afda397a51135b9b531a20cb78e7db6a8a41b
M3chanix <M3chanix@protonmail.com>
2022-06-30 09:21:19 +0300

    all tests done

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,64 +1,55 @@
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from hw4.MainPage import MainPage
-from hw4.CatalogPage import CatalogPage
-from hw4.ProductPage import ProductPage
-from hw4.AdminPage import AdminPage
-from hw4.RegisterPage import RegisterPage
+from hw5.MainPage import MainPage
+from hw5.CatalogPage import CatalogPage
+from hw5.ProductPage import ProductPage
+from hw5.AdminPage import AdminPage
+from hw5.RegisterPage import RegisterPage
+from hw5.AdminCatalogPage import AdminCatalogPage
+from hw5.LoginPage import LoginPage
 
 
 def test_main_page(driver):
-    main_page = 'http://192.168.0.190:8081/'
-    driver.get(main_page)
-    driver.find_element(*MainPage.CURRENCY_SWITCH)
-
-    wait = WebDriverWait(driver, 5)
-    wait.until(EC.visibility_of_element_located(MainPage.SLIDE_PANNEL))
-
-    driver.find_element(*MainPage.SUPPORT_PHONE)
-    driver.find_element(*MainPage.CART_BUTTON)
-    driver.find_element(*MainPage.FEATURED_LIST)
-
+    MainPage(driver).verify_main_page()
 
 def test_catalog_page(driver):
-    catalog_page = 'http://192.168.0.190:8081/laptop-notebook'
-    driver.get(catalog_page)
-    driver.find_element(*CatalogPage.BREADCRUMB)
-    driver.find_element(*CatalogPage.PRODUCT_LIST)
-    driver.find_element(*CatalogPage.FIRST_PRODUCT)
-    driver.find_element(*CatalogPage.LIMIT_INPUT)
-    driver.find_element(*CatalogPage.SORT_INPUT)
+    CatalogPage(driver).verify_catalog_page()
 
 def test_product_page(driver):
-    product_page = 'http://192.168.0.190:8081/laptop-notebook/hp-lp3065'
-    driver.get(product_page)
-    driver.find_element(*ProductPage.PRICE)
-    driver.find_element(*ProductPage.WISHLIST_BUTTON)
-    driver.find_element(*ProductPage.THUMBNAIL)
-    driver.find_element(*ProductPage.ADD_CART_BUTTON)
-    
-    wait = WebDriverWait(driver, 2)
-    wait.until(EC.visibility_of_element_located(ProductPage.RATING_STARS))
+    ProductPage(driver).verify_product_page()
 
 def test_admin_page(driver):
-    admin_page = 'http://192.168.0.190:8081/admin'
-    driver.get(admin_page)
-    driver.find_element(*AdminPage.NAVBAR_IMAGE)
-    driver.find_element(*AdminPage.USERNAME_INPUT)
-    driver.find_element(*AdminPage.PASSWORD_INPUT)
-    driver.find_element(*AdminPage.FORGOT_LINK)
-    driver.find_element(*AdminPage.LOGIN_BUTTON)
-    
+    AdminPage(driver).verify_admin_page()
 
 def test_register_page(driver):
-    register_page = 'http://192.168.0.190:8081/index.php?route=account/register'
-    driver.get(register_page)
-    driver.find_element(*RegisterPage.FIRST_NAME_INPUT)
-    driver.find_element(*RegisterPage.LOGIN_PAGE_LINK)
-    driver.find_element(*RegisterPage.PASSWORD_INPUT)
-    driver.find_element(*RegisterPage.SUBSCRIBE_YES_CHECKBOX)
-    driver.find_element(*RegisterPage.CONTINUE_BUTTON)
+    RegisterPage(driver).verify_register_page()
 
+
+
+def test_add_product(driver):
+    AdminPage(driver).login_as_admin('user', 'bitnami')
+    product_name = AdminCatalogPage(driver).add_new_product('new_laptop_1', 'new_model')
+    CatalogPage(driver).verify_product(product_name)
+
+def test_delete_product(driver):
+    AdminPage(driver).login_as_admin('user', 'bitnami')
+    AdminCatalogPage(driver).delete_product('new_laptop_1')
+    assert not CatalogPage(driver).verify_product('new_laptop_1')
+
+def test_register_new_user(driver):
+    user = {'first_name': 'first_name', 
+            'last_name': 'last_name', 
+            'email': 'test@user.com', 
+            'phone': '88005555555', 
+            'password': 'test_password'}
+    
+    RegisterPage(driver).register_new_user(**user)
+    LoginPage(driver).login_as(user['email'], user['password'])
+
+def test_change_currency(driver):
+    prev_currency = MainPage(driver).get_current_currency()
+    MainPage(driver).change_currency()
+    active_currency = MainPage(driver).get_current_currency()
+    assert active_currency != prev_currency
 
